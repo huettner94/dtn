50ae5e14dd1b666f4cb0d78f9ce0305b83dc092f
Felix Huettner <huettner94@gmx.de>
2022-03-31 10:41:50 +0200

    move protobuf definitions to top level

--- dtrd/build.rs
+++ dtrd/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/bundle/bundle.proto")?;
+    tonic_build::compile_protos("../protobuf/bundle/bundle.proto")?;
     Ok(())
 }
